a805459e807c7c9d2187e6d98e91d3596e2d3865
Jędrzej Kuczyński <jk.kuczynski@gmail.com>
2017-04-08 17:48:39 +0200

    Dotplot

---
+++ BSB/Lab05/dotplot.py
@@ -0,0 +1,38 @@
+"""seq1 = "ACGCAGTA"
+seq2 = "ACGGATA" 
+dotplot = [["." for i in range(len(seq2))] for j in range(len(seq1))]
+
+for i in range(len(dotplot)):
+	for j in range(len(dotplot[i])):
+		if seq1[i] == seq2[j]:
+			dotplot[i][j] = "X"
+
+print("  ", end = "")
+for char in seq2:
+	print(char, end = " ")
+print()
+for i in range(len(dotplot)):
+	print(seq1[i], " ".join(dotplot[i]))"""
+
+seq1 = "ABRACADABRACADABRA"
+seq2 = "ABRACADABRACADABRA"
+threshold = 2
+window = 3
+cross = window//2
+dotplot = [["." for i in range(len(seq2))] for j in range(len(seq1))]
+
+for i in range(len(dotplot) - window + 1):
+	for j in range(len(dotplot[i]) - window + 1):
+		counter = 0
+		for k in range(window):
+			if seq1[i+k] == seq2[j+k]:
+				counter += 1
+		if counter >= threshold:
+			dotplot[i+cross][j+cross] = 'X'
+
+print("  ", end = "")
+for char in seq2:
+	print(char, end = " ")
+print()
+for i in range(len(dotplot)):
+	print(seq1[i], " ".join(dotplot[i]))
