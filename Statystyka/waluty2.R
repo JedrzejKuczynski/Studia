1af792390e6cfe4ab5397ed75e86d887c2f12b5c
Jędrzej Kuczyński <jk.kuczynski@gmail.com>
2017-06-16 17:21:11 +0200

    Prace stoja

--- Statystyka/waluty2.R
+++ Statystyka/waluty2.R
@@ -2,7 +2,7 @@ library(readxl)
 library(xlsx)
 
 # input <- function() { 
-#   # Funkcja odpowiedzialna za informacje podawane przez u¿ytkownika.
+#   # Funkcja odpowiedzialna za informacje podawane przez u?ytkownika.
 #   currency <- readline(prompt = "Prosze wpisac walute: ")
 #   date_b <- readline(prompt = "Od (prosze wpisac w formacie YYYY-MM-DD): ")
 #   date_e <- readline(prompt = "Do prosze wpisac w formacie YYYY-MM-DD): ")
@@ -12,7 +12,7 @@ library(xlsx)
 url_data <- c("USD", "1996-01-01", "2000-06-01")
 
 download_data <- function(url_data) { 
-  # Funkcja odpowiedzialna za œci¹ganie danych z serisu NBP.
+  # Funkcja odpowiedzialna za ?ci?ganie danych z serisu NBP.
   
   urlb <- "http://www.nbp.pl/kursy/Archiwum/archiwum_tab_a_"
   col_types <- col_t <- c("date", rep("text", 26))
@@ -26,9 +26,9 @@ download_data <- function(url_data) {
   
   for(i in 1:n) {
     yi <- y[i]
-    url <- paste0(urlb, yi, ".xls") # Sk³adamy odpowiedni URL
+    url <- paste0(urlb, yi, ".xls") # Sk?adamy odpowiedni URL
     destfile <- paste0(yi, ".xls") # Tworzymy plik Excela
-    download.file(url, destfile, mode = "wb") # Œci¹gamy do niego dane
+    download.file(url, destfile, mode = "wb") # ?ci?gamy do niego dane
     
     if(yi < 2000) {
       table <- read_excel(destfile, skip = 1) # Odpowiednio go czytamy.
@@ -50,7 +50,7 @@ download_data <- function(url_data) {
       col_t <- c(col_types, rep("text", 13))
       col_t[39] <- "numeric"
       table <- read_excel(destfile, col_types = col_t) 
-      # Wewnêtrzne kodowanie Excela, trzeba zmieniæ typy kolumn.
+      # Wewn?trzne kodowanie Excela, trzeba zmieni? typy kolumn.
     } else if(yi == 2014) {
       col_t <- c(col_types, rep("text", 12))
       col_t[38] <- "numeric"
@@ -67,8 +67,8 @@ download_data <- function(url_data) {
 
 ###############################################################################
 
-# url_data <- input() # Uzyskujemy informacje od u¿ytkownika.
-USDdata <- download_data(url_data) # Œci¹gamy dane z serwisu NBP.
+# url_data <- input() # Uzyskujemy informacje od u?ytkownika.
+USDdata <- download_data(url_data) # ?ci?gamy dane z serwisu NBP.
 
 library(lubridate)
 str(USDdata)
@@ -87,17 +87,3 @@ index_df <- 1
 dates <- c()
 values <- c()
 indices <- c()
-
-for(i in as.numeric(year_b):as.numeric(year_e)){
-  if(i >= 1996 && i < 2000){
-    df <- data[[index_df]]
-    index_dateb <- NULL
-    for(j in 1:nrow(df)){
-      if(df[[j,2]] >= date_b){
-        index_dateb <- j
-        break
-      }
-    }
-  }
-  index_df <- index_df + 1
-}
